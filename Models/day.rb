77e93096a320341ffe407e719b9e17bece5c1951
michal_urbanski <michal_urbanski@windowslive.com>
2016-05-21 20:29:06 +0200

    Adds handling of date to day class

--- Models/day.rb
+++ Models/day.rb
@@ -1,5 +1,6 @@
+require 'date'
 class Day
-  attr_reader :name, :actions
+  attr_reader :name, :actions, :date
 
   # TODO: you can't have two constructors in ruby?
   #def initialize(name, actions)
@@ -10,6 +11,7 @@ class Day
   def initialize(name)
     @name = name
     @actions = Array.new
+    @date = extract_date_from_day_name(name)
   end
 
   def add_action(text)
@@ -21,5 +23,11 @@ class Day
     @actions.each do |action|
       puts(action)
     end
+  end
+
+  private
+  def extract_date_from_day_name(name)
+    datePart = name.split[1]
+    @date = Date.strptime(datePart, '%Y-%m-%d') # TODO: move to module as date format
   end
 end
--- Tests/day_tests.rb
+++ Tests/day_tests.rb
@@ -3,9 +3,16 @@ require_relative '../Models/day'
 
 class DayTests < Test::Unit::TestCase
   def test_create_day_using_simple_constructor
-    day = Day.new("day")
+    day = Day.new("Monday 2016-05-01")
 
-    assert_equal("day", day.name)
+    assert_equal("Monday 2016-05-01", day.name)
+  end
+
+  def test_day_should_have_date
+    date = "2016-05-01"
+    day = Day.new("Monday" + " " + date)
+
+    assert_equal(Date.parse(date), day.date)
   end
 
   #def test_create_day_using_two_parameters_constructor
