b2c29363663433d8e3b973ca341b9d718417f8b4
michal_urbanski <michal_urbanski@windowslive.com>
2016-07-02 22:22:27 +0200

    Uses logger instead of simple message printing for FileReader

--- FileHandling/file_reader.rb
+++ FileHandling/file_reader.rb
@@ -7,38 +7,33 @@ class FileReader
 
   def initialize(filePath)
     @filePath = filePath
-    logger.info("starting logging")
   end
 
   def read_file
-    print_message("Reading file #{@filePath}")
+    logger.info "Reading file #{@filePath}"
 
     check_if_file_exists
     @lines = read_lines
 
-    print_message("Reading file finished")
+    logger.info "Reading file finished"
   end
 
   private
   def check_if_file_exists
     if File.exists?(@filePath) then
       if File.directory?(@filePath) then
-        print_message "File #{@filePath} is a directory"
+        logger.info "File #{@filePath} is a directory"
         exit
       else
-        print_message "Reading file..."
+        logger.info "Reading file..."
       end
     else
-      print_message "File #{@filePath} does not exist"
+      logger.info "File #{@filePath} does not exist"
       exit
     end
   end
 
   def read_lines
     IO.readlines(@filePath)
-  end
-
-  def print_message(message)
-    puts message
   end
 end
