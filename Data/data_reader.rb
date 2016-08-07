20aafd4acca4fab62172c284906839fe673c7dee
michal_urbanski <michal_urbanski@windowslive.com>
2016-08-07 22:59:37 +0200

    Further refactoring of data_reader

--- Data/data_reader.rb
+++ Data/data_reader.rb
@@ -1,18 +1,20 @@
 require_relative '../FileHandling/file_reader'
 
 class DataReader
+  attr_reader :lines
+
   def initialize(path)
     @path = path
-  end
-
-  def absolute_path
-    File.expand_path(@path, __FILE__)
   end
 
   def read_data
     fileReader = FileReader.new(absolute_path)
     fileReader.read_file
+    @lines = fileReader.lines
+  end
 
-    fileReader.lines
+  private
+  def absolute_path
+    File.expand_path(@path, __FILE__)
   end
 end
--- Tests/DataReaderTests/data_reader_tests.rb
+++ Tests/DataReaderTests/data_reader_tests.rb
@@ -5,17 +5,23 @@ require_relative '../../Data/test_data'
 describe TestData do
   describe "read test data" do
     it "data read from test file is not null" do
-      test_data = TestData.new
-      data = test_data.read_data
-      data.wont_be_nil
+      test_data = act
+
+      test_data.lines.wont_be_nil
     end
 
     # We do not differentiate now between days
     # but file can't have 7 days if it doesn't have at least 7 lines
     it "data in test file must have at least 7 entries" do
-      test_data = TestData.new
-      data = test_data.read_data
-      data.length.must_be :>=, 7
+      test_data = act
+
+      test_data.lines.length.must_be :>=, 7
     end
+  end
+
+  def act
+    test_data = TestData.new
+    test_data.read_data
+    test_data
   end
 end
