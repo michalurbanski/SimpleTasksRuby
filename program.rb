3519ab4fff7a718618a160e34aa9f63b1952058c
michal_urbanski <michal_urbanski@windowslive.com>
2016-09-01 21:32:44 +0200

    Loads current path to LOAD_PATH env variable

--- program.rb
+++ program.rb
@@ -5,17 +5,6 @@ require_relative 'main'
 require_relative 'Common/console_writer'
 require_relative 'Modules/console_module'
 
-# Method must be declared above its usage
-def load_libraries_to_load_path
-  #$LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..')
-
-  puts(File.join(File.dirname(__FILE__), '..', 'lib'))
-
-  puts "Load path is"
-  puts $LOAD_PATH
-end
-
-load_libraries_to_load_path
 main = Main.new(ConsoleWriter.new)
 
 ConsoleModule.clear_screen
