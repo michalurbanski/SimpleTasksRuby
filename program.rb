831cada1a3eb86fad0c27924909406f84d8c6469
michal_urbanski <michal_urbanski@windowslive.com>
2016-09-04 16:45:24 +0200

    Cleans up require statements in program file

--- program.rb
+++ program.rb
@@ -1,9 +1,9 @@
 require 'rubygems'
 require 'bundler/setup'
 
-require_relative 'main'
-require_relative 'Common/console_writer'
-require_relative 'Modules/console_module'
+require 'lib/simple_tasks_ruby/main'
+require 'lib/simple_tasks_ruby/Common/console_writer'
+require 'lib/simple_tasks_ruby/Modules/console_module'
 
 main = Main.new(ConsoleWriter.new)
 
