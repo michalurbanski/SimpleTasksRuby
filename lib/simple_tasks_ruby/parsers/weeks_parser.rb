a38f4b3cdd7a8f8d6adbdd2f77089dd1928ff802
michal_urbanski <michal_urbanski@windowslive.com>
2016-12-03 17:44:43 +0100

    Week parser tests

--- lib/simple_tasks_ruby/parsers/weeks_parser.rb
+++ lib/simple_tasks_ruby/parsers/weeks_parser.rb
@@ -1,12 +1,12 @@
-# Finds lines with weeks
+# Determines if line is a week line
 module WeeksParser
   def self.is_line_with_week(line)
     # Line is a week indication when it has:
     # - "Week" word (case-insensitive)
     # - One or more digits after Week
-    # Examples: Week 1, Week 42
+    # Examples: Week 1, Week 42, week 50
 
-    #TODO: implement
-    true
+    pattern = /^Week\s\d+\s*/i
+    line =~ pattern ? true : false
   end
 end
--- spec/simple_tasks_ruby/parsers/weeks_parser_spec.rb
+++ spec/simple_tasks_ruby/parsers/weeks_parser_spec.rb
@@ -12,21 +12,37 @@ describe WeeksParser do
     end
 
     it "Line with week above 10 is treated as a week" do
+      weeks = ["week 10", "Week 99"]
 
+      result = weeks.all? {|week| WeeksParser.is_line_with_week(week)}
+
+      result.must_equal true
     end
 
     it "Line with week is case-insensitive" do
+      line = "week 9"
 
+      result = WeeksParser.is_line_with_week line
+
+      result.must_equal true
     end
   end
 
   describe "Invalid week lines" do
     it "Line with only number is not a week" do
+      line = "12"
 
+      result = WeeksParser.is_line_with_week line
+
+      result.must_equal false
     end
 
     it "Line does not have correct week indication" do
+      line = "this is not a week"
 
+      result = WeeksParser.is_line_with_week line
+
+      result.must_equal false
     end
   end
 end
