8bb5beb7a9501e591cb917655bc195f1a452634a
michal_urbanski <michal_urbanski@windowslive.com>
2017-01-08 20:50:58 +0100

    Rewrites day tests to spec

--- spec/simple_tasks_ruby/other/day_spec.rb
+++ spec/simple_tasks_ruby/other/day_spec.rb
@@ -1,42 +1,50 @@
 require 'spec_helper'
 
-class DayTests < Test::Unit::TestCase
-  def test_create_day_using_simple_constructor
-    day = Day.new("Monday 2016-05-01")
+describe Day do
+  describe "Day tests" do
+    before do
+      @name = "Monday 2016-05-01"
+    end
 
-    assert_equal("Monday 2016-05-01", day.name)
-  end
+    it "Created day has name" do
+      day = Day.new(@name)
 
-  def test_day_has_date
-    date = "2016-05-01"
-    day = Day.new("Monday" + " " + date)
+      day.name.must_equal @name
+    end
 
-    assert_equal(Date.parse(date), day.date)
-  end
+    it "Day has proper date" do
+      day = Day.new(@name)
 
-  def test_day_without_date_throws_exception
-    assert_raise NoDateException do
-      day = Day.new("Monday")
+      day.date.must_equal Date.parse("2016-05-01")
     end
   end
 
-  def test_day_has_tasks_equal_to_actions
-    day = Day.new("Monday 2016-05-12")
-    day.add_action("- First action")
-    day.add_action("- delayed, - Second action")
+  describe "Day incorrect data" do
+    it "Day not initialized with date can't be created" do
+      lambda {
+        day = Day.new("Monday")
+      }.must_raise NoDateException
+    end
 
-    daily_status_extractor = DailyStatusExtractor.new(day)
-    daily_status_extractor.proceed_day
-
-    dailyTasks = daily_status_extractor.daily_tasks
-    day.add_tasks(dailyTasks)
-
-    assert_equal(day.actions.length, day.tasks.length)
-  end
-
-  def test_date_cant_be_parsed_throws_exception_with_meaningfull_information
-    assert_raise IncorrectDateForDayException do
-      day = Day.new("Monday 2017-01-44")
+    it "Day initialized with not correct date can't be created" do
+      lambda {
+        day = Day.new("Monday 2016-05-44")
+      }.must_raise IncorrectDateForDayException
     end
   end
 end
+
+#   def test_day_has_tasks_equal_to_actions
+#     day = Day.new("Monday 2016-05-12")
+#     day.add_action("- First action")
+#     day.add_action("- delayed, - Second action")
+
+#     daily_status_extractor = DailyStatusExtractor.new(day)
+#     daily_status_extractor.proceed_day
+
+#     dailyTasks = daily_status_extractor.daily_tasks
+#     day.add_tasks(dailyTasks)
+
+#     assert_equal(day.actions.length, day.tasks.length)
+#   end
+
