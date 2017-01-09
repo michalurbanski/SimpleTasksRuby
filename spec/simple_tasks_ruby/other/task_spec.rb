511c6a82275ddc21b3582ca5dfdb89ca86299866
michal_urbanski <michal_urbanski@windowslive.com>
2017-01-09 22:13:48 +0100

    Improves tests for tasks

--- spec/simple_tasks_ruby/other/task_spec.rb
+++ spec/simple_tasks_ruby/other/task_spec.rb
@@ -1,8 +1,26 @@
-class TaskTests < Test::Unit::TestCase
-  def test_task_after_creation_all_members_are_nil
-    task = Task.new("- this is task", Date.new(2016, 1, 1))
+require 'spec_helper'
 
-    assert_not_nil(task.title)
-    assert_equal(Date.new(2016, 1, 1), task.originalDate)
+describe Task do
+  before do
+      @date = Date.new(2016,1, 1)
+      @title = "- this is task"
+  end
+
+  describe "Base task" do
+    it "Task when created has basic properties filled" do
+      task = Task.new(@title, @date)
+
+      task.title.wont_be_nil
+      task.title.must_equal @title
+      task.originalDate.must_equal @date
+    end
+  end
+
+  describe "Delayed task" do
+    it "Delayed tasks is a task" do
+      delayedTask = DelayedTask.new(@title, @date)
+
+      delayedTask.is_a?(Task).must_equal true
+    end
   end
 end
