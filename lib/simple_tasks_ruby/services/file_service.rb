d2bd761dbff6d70186949870a085a410769fba3f
michal_urbanski <michal_urbanski@windowslive.com>
2017-03-09 20:36:38 +0100

    FileService class - dependencies cleanup

--- lib/simple_tasks_ruby/services/file_service.rb
+++ lib/simple_tasks_ruby/services/file_service.rb
@@ -1,7 +1,8 @@
 # Manages files handling in application
 class FileService
-  def initialize(writer)
+  def initialize(writer, file_reader)
     @writer = writer #TODO: currently not used. Should this class print results itself?
+    @file_reader = file_reader
   end
 
   def get_lines_from_file(path)
@@ -11,12 +12,12 @@ class FileService
     #       :path => path
     #     })
   
-    Array.new()
+    # Array.new()
 
 
     # reader = FileReader.new(path)
 
-    # reader.read
-    # @file_lines = reader.lines
+    @file_reader.read # perform action 
+    @file_reader.lines # get state of the object
   end
 end
--- spec/simple_tasks_ruby/services/file_service_spec.rb
+++ spec/simple_tasks_ruby/services/file_service_spec.rb
@@ -4,14 +4,18 @@ describe FileService do
   describe "Reads lines from file" do 
     it "When path is empty then raises exception" do 
       writer = mock()
-      @file_service = FileService.new(writer)
+      file_reader = mock()
+      @file_service = FileService.new(writer, file_reader)
       
       lambda {@file_service.get_lines_from_file(nil)}.must_raise ArgumentError
     end
 
     it "When path is not empty then returns lines" do 
       writer = mock() 
-      @file_service = FileService.new(writer)
+      file_reader = mock()
+      file_reader.expects(:read)
+      file_reader.expects(:lines).returns(Array.new())
+      @file_service = FileService.new(writer, file_reader)
 
       lines = @file_service.get_lines_from_file("valid path")
       
