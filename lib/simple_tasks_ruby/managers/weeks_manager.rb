ee37c5d7ace451fea526cc65b84c07661465a531
michal_urbanski <michal_urbanski@windowslive.com>
2017-03-19 20:11:18 +0100

    Adds first tests for weeks manager

--- lib/simple_tasks_ruby/managers/weeks_manager.rb
+++ lib/simple_tasks_ruby/managers/weeks_manager.rb
@@ -1,3 +1,9 @@
 class WeeksManager
+  attr_reader :weeks
 
+  def convert_data_to_weeks(lines) 
+    return nil if lines.nil? || lines.empty?
+  
+    @weeks = Array.new
+  end
 end
--- spec/simple_tasks_ruby/managers/weeks_manager_spec.rb
+++ spec/simple_tasks_ruby/managers/weeks_manager_spec.rb
@@ -1,7 +1,40 @@
 require "spec_helper" 
 
+# This test reads test data from disk
 describe WeeksManager do
-  describe "Manages tasks" do 
+  before do 
+    writer = mock()
+    data_reader = FileSystemDataReader.new({
+      :path => TestData.default_path
+    })  
 
+    file_service = FileService.new(writer, data_reader) 
+    @lines = file_service.get_lines_from_file
+  end
+
+  describe "Valid cases" do 
+    it "Creates weeks when input lines are not empty" do 
+      weeks_manager = WeeksManager.new
+      weeks_manager.convert_data_to_weeks(@lines) 
+
+      weeks_manager.weeks.wont_be_nil 
+    end 
+  end 
+
+  describe "Invalid cases" do 
+    it "Weeks are nil for empty input" do 
+      lines = Array.new
+      weeks_manager = WeeksManager.new 
+      weeks_manager.convert_data_to_weeks(lines)
+
+      weeks_manager.weeks.must_be_nil
+    end 
+
+    it "Weeks are nil for nil input" do 
+      weeks_manager = WeeksManager.new
+      weeks_manager.convert_data_to_weeks(nil) 
+
+      weeks_manager.weeks.must_be_nil
+    end
   end 
 end
