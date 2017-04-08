9cce7aaca5964f0457e5d25cd1b4f262da0472bb
michal_urbanski <michal_urbanski@windowslive.com>
2017-04-08 22:55:57 +0200

    Cleans up tests

--- spec/simple_tasks_ruby/converters/lines_to_weeks_converter_spec.rb
+++ spec/simple_tasks_ruby/converters/lines_to_weeks_converter_spec.rb
@@ -7,8 +7,6 @@ describe LinesToWeeksConverter do
 
   describe "Converts correct input to weeks" do 
     it "Converts lines to week" do 
-      skip("Temporary skipped")
-
       lines = Array.new
       lines << "Week 1"
       lines << "Monday 2015-01-01"
@@ -16,9 +14,30 @@ describe LinesToWeeksConverter do
       lines << "- second task"
       lines << "Tuesday 2015-01-02"
 
-      converter = LinesToWeeksConverter.new
-      weeks = converter.convert(lines) 
+      # converter = LinesToWeeksConverter.new
+      weeks = @converter.convert(lines) 
+
+      weeks.length.must_equal 1
+      weeks[0].days.length.must_equal 2
     end 
+
+    it "Converts lines with multiple weeks to weeks objects" do 
+      lines = Array.new 
+      lines << "Week 1" 
+      lines << "Monday 2015-01-01" 
+      lines << ""
+      lines << "--------" 
+      lines << "week 2" 
+      lines << "Monday 2015-01-08" 
+      lines << "--------" 
+      lines << "--------" 
+
+      weeks = @converter.convert(lines) 
+
+      weeks.length.must_equal 2
+      weeks.first.days.length.must_equal 1
+      weeks.last.days.length.must_equal 1
+    end
   end
 
   describe "Handles incorrect input" do 
@@ -35,56 +54,3 @@ describe LinesToWeeksConverter do
     end
   end
 end
-
-# describe LinesToWeeksConverter do
-#   describe "Converts lines to weeks correctly" do
-#     before do
-#       @week_lines = ["Week 1", "Week 2", "Week 3"]
-#       @days_for_first_week = ["Monday 2016-12-05", "- first task", "Tuesday 2016-12-06"]
-#       @days_for_second_week = ["Monday 2016-12-12", "- delayed, - second task"]
-
-#       @lines_to_weeks_converter = LinesToWeeksConverter.new
-#     end
-
-#     # it "Weeks are properly parsed" do
-#     #   weeks = @lines_to_weeks_converter.convert(@week_lines)
-
-#     #   weeks.length.must_equal(@week_lines.length)
-#     # end
-
-#     # it "Weeks with first week having days is properly parsed" do
-#     #   lines = Array.new(@week_lines)
-#     #   lines.insert(1, @days_for_first_week).flatten! # first week has days
-
-#     #   weeks = @lines_to_weeks_converter.convert(lines)
-
-#     #   weeks.length.must_equal(@week_lines.length)
-#     #   weeks[@week_lines[0]].length.must_equal(@days_for_first_week.length)
-#     # end
-
-#     # it "Weeks with days for each one are properly parsed" do
-#     #   lines = Array.new(@week_lines)
-#     #   lines.insert(1, @days_for_first_week)
-#     #   lines.insert(3, @days_for_second_week)
-#     #   lines.flatten! # first and second week have days with actions
-
-#     #   weeks = @lines_to_weeks_converter.convert(lines)
-
-#     #   weeks.length.must_equal(@week_lines.length)
-#     #   weeks[@week_lines[0]].length.must_equal(@days_for_first_week.length)
-#     #   weeks[@week_lines[1]].length.must_equal(@days_for_second_week.length)
-#     # end
-
-#     # it "Nil input collection of lines causes runtime error" do
-#     #   lambda {
-#     #       @lines_to_weeks_converter.convert(nil)
-#     #     }.must_raise ArgumentError
-#     # end
-
-#     # it "Empty input collection of lines causes runtime error" do
-#     #   lambda {
-#     #       @lines_to_weeks_converter.convert(Array.new)
-#     #     }.must_raise ArgumentError
-#     # end
-#   end
-# end
