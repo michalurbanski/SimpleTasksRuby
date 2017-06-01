90878f66f38aac0895658c2e00221db29baafc8b
michal_urbanski <michal_urbanski@windowslive.com>
2017-06-01 21:27:06 +0200

    Implements checking if lines are array of strings

--- lib/simple_tasks_ruby/modules/empty_lines_removal_module.rb
+++ lib/simple_tasks_ruby/modules/empty_lines_removal_module.rb
@@ -3,7 +3,7 @@ module EmptyLinesRemovalModule
 
   # Works only for arrays
   def remove_blank_lines(lines) 
-    raise ArgumentError.new("Array must be provided") unless lines.kind_of?(Array) 
+    raise ArgumentError.new("Array of strings must be provided") unless ArrayModule.array_of_strings?(lines) 
 
     lines -= ["\n", "", ''] # deletes element from source array and assigns it to the same variable
   end
