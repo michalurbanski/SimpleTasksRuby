f8750d1fa95a46c5f1283d46bc09ea62bf3ffd8a
michal_urbanski <michal_urbanski@windowslive.com>
2017-06-01 21:23:21 +0200

    Module for checking if array of strings

--- lib/simple_tasks_ruby/modules/array_module.rb
+++ lib/simple_tasks_ruby/modules/array_module.rb
@@ -4,4 +4,15 @@ module ArrayModule
   def convert_to_hash_with_indices(array) 
     Hash[(1...array.size+1).zip array]
   end
+
+  def array_of_strings?(array) 
+    return false unless array.kind_of?(Array)
+
+    # We have array - check if this is an array of strings
+    array.each do |elem|
+      return false unless elem.kind_of?(String)
+    end 
+
+    return true
+  end
 end
--- spec/simple_tasks_ruby/modules/array_module_spec.rb
+++ spec/simple_tasks_ruby/modules/array_module_spec.rb
@@ -1,14 +1,45 @@
 require 'spec_helper' 
 
 describe ArrayModule do 
-  it 'Converts array to hash' do 
-    a = Array.new
-    a << "line1" 
-    a << "line2" 
 
-    result = ArrayModule.convert_to_hash_with_indices(a) 
+  describe 'Convert to hash' do 
+    it 'Converts array to hash' do 
+      a = ["line1", "line2"]
 
-    result.keys[0].must_equal 1
-    result.keys[1].must_equal 2
+      result = ArrayModule.convert_to_hash_with_indices(a) 
+
+      result.keys[0].must_equal 1
+      result.keys[1].must_equal 2
+    end
+  end
+
+  describe 'Is array of strings' do 
+    it 'Object is not array of strings' do 
+      a = Object.new
+
+      result = act(a)
+
+      result.must_equal false
+    end
+
+    it 'Checks if array of strings' do 
+      a = ["line1", "line2"]
+
+      result = act(a)
+
+      result.must_equal true
+    end
+
+    it 'Array with objects is not array of strings' do 
+      a = [Object.new]
+
+      result = act(a)
+
+      result.must_equal false
+    end
+
+    def act(array) 
+      ArrayModule.array_of_strings?(array)
+    end
   end
 end
