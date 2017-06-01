6a2aa2d23a778505a6db132dbd7f53dbb98233b3
michal_urbanski <michal_urbanski@windowslive.com>
2017-06-01 20:53:04 +0200

    Improves module

--- lib/simple_tasks_ruby/modules/empty_lines_removal_module.rb
+++ lib/simple_tasks_ruby/modules/empty_lines_removal_module.rb
@@ -1,7 +1,10 @@
 module EmptyLinesRemovalModule 
   extend self
 
+  # Works only for arrays
   def remove_blank_lines(lines) 
+    raise ArgumentError.new("Array must be provided") unless lines.kind_of?(Array) 
+
     lines -= ["\n", "", ''] # deletes element from source array and assigns it to the same variable
   end
 end
--- spec/simple_tasks_ruby/modules/empty_lines_removal_module_spec.rb
+++ spec/simple_tasks_ruby/modules/empty_lines_removal_module_spec.rb
@@ -19,5 +19,13 @@ describe EmptyLinesRemovalModule do
       without_blank.count.must_equal(1) 
       without_blank[0].must_equal('test')
     end
+
+    it "Cannot remove empty lines from object" do 
+      object = Object.new
+
+      lambda{
+        without_blank = EmptyLinesRemovalModule.remove_blank_lines(object)
+      }.must_raise ArgumentError
+    end
   end 
 end
