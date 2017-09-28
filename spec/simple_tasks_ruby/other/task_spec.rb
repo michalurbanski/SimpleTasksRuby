7668fe90d27f158aa0345abcb52b142b1b1de2d9
michal_urbanski <michal_urbanski@windowslive.com>
2017-09-28 20:19:08 +0200

    Merges tests for tasks into one file

--- spec/simple_tasks_ruby/other/task_spec.rb
+++ spec/simple_tasks_ruby/other/task_spec.rb
@@ -1,13 +1,17 @@
 require 'spec_helper'
 
+# NOTE: If any module needs to be included, it has to be included before describe (not inside it).
+include SimpleTasksRuby::TaskType
+
 describe Task do
   subject { task }
 
   let(:date) { Date.new(2016, 1, 1) }
   let(:title) { "- this is task" }
-  let(:task) { Task.new(title, date) } 
-
+  
   describe "Base task" do
+    let(:task) { Task.new(title, date) } 
+
     it "When base task is created it has date and title" do
       subject.title.wont_be_nil
       subject.title.must_equal title
@@ -16,6 +20,37 @@ describe Task do
 
     it "Base task has no additional properties" do 
       subject.status.must_be_nil
+      subject.done_date.must_be_nil
+    end
+  end
+
+  describe "Done task" do
+    let(:task) do 
+      Task.new(title, date, { status: DONE })
+    end
+
+    it "Done task has status set" do 
+      subject.status.must_equal DONE
+      subject.done_date.must_be_nil
+    end
+  end
+
+  describe "Delayed and then done task" do
+    it "Task had been delayed but it was done - has correct status and completion date" do
+      done_date = date + 2
+      subject = Task.new(title, date, 
+        { status: DELAYED_DONE, done_date: done_date })
+      
+      subject.status.must_equal DELAYED_DONE
+      subject.done_date.must_equal done_date
+    end
+  end
+
+  describe "Delayed task" do
+    it "Delayed task has correct stauts but no done date" do
+      subject = Task.new(title, date, { status: DELAYED })
+
+      subject.status.must_equal DELAYED
       subject.done_date.must_be_nil
     end
   end
