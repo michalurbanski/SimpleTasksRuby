17ab555e04c791df037f6a90768ffa1c3113ed62
michal_urbanski <michal_urbanski@windowslive.com>
2017-10-08 18:35:18 +0200

    Refactors code

--- lib/simple_tasks_ruby/managers/tasks_manager.rb
+++ lib/simple_tasks_ruby/managers/tasks_manager.rb
@@ -4,20 +4,18 @@ class TasksManager
 
     find_delayed_tasks(tasks)
   end
-
-  def find_tasks_by_status(tasks, task_status)
-    return nil if ArrayModule.is_nil_or_empty?(tasks)
-
-    tasks.select { |task| task.status == task_status } 
-  end
   
-  # TODO: Obsolete - to be removed
-  # TODO: When removed then collect_tasks_for_all_weeks method needs to be exposed as public
   def find_delayed_tasks(tasks)
     find_tasks_by_status(tasks, SimpleTasksRuby::TaskType::DELAYED)
   end
 
-  private 
+  private
+    def find_tasks_by_status(tasks, task_status)
+      return nil if ArrayModule.is_nil_or_empty?(tasks)
+
+      tasks.select { |task| task.status == task_status } 
+    end
+
     def collect_tasks_for_all_weeks(weeks)
       tasks = Array.new 
 
