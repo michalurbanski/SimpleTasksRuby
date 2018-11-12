f5078047df81dd2e116302924913300efb484197
michal_urbanski <michal_urbanski@windowslive.com>
2018-11-12 20:10:39 +0100

    Encloses another class in module

--- lib/simple_tasks_ruby/services/file_service.rb
+++ lib/simple_tasks_ruby/services/file_service.rb
@@ -1,13 +1,15 @@
-# Manages files handling in application
-class FileService
-  def initialize(file_reader)
-    @file_reader = file_reader
-  end
+# Manages reading input files with lines to proceed
+module SimpleTasksRuby
+  class FileService
+    def initialize(file_reader)
+      @file_reader = file_reader
+    end
 
-  def read_lines_from_file
-    raise ArgumentError.new "Reader is not initialized" if @file_reader.nil?
-    
-    @file_reader.read
-    @file_reader.lines
+    def read_lines_from_file
+      raise ArgumentError.new "Reader is not initialized" if @file_reader.nil?
+      
+      @file_reader.read
+      @file_reader.lines
+    end
   end
 end
--- spec/simple_tasks_ruby/services/file_service_spec.rb
+++ spec/simple_tasks_ruby/services/file_service_spec.rb
@@ -1,24 +1,26 @@
 require 'spec_helper' 
 
-describe FileService do 
-  describe "Reads lines from file" do 
-    it "When reader is not initialized then exception is thrown" do 
-      @file_service = FileService.new(nil)
-      
-      lambda {@file_service.read_lines_from_file}.must_raise ArgumentError
-    end
+module SimpleTasksRuby
+  describe FileService do 
+    describe "Reads lines from file" do 
+      it "When reader is not initialized then exception is thrown" do 
+        @file_service = FileService.new(nil)
+        
+        lambda {@file_service.read_lines_from_file}.must_raise ArgumentError
+      end
 
-    it "When path is not empty then returns lines" do 
-      data = Array.new
-      file_reader = mock()
-      file_reader.expects(:read)
-      file_reader.expects(:lines).returns(data)
-      @file_service = FileService.new(file_reader)
+      it "When path is not empty then returns lines" do 
+        data = Array.new
+        file_reader = mock()
+        file_reader.expects(:read)
+        file_reader.expects(:lines).returns(data)
+        @file_service = FileService.new(file_reader)
 
-      lines = @file_service.read_lines_from_file
-      
-      lines.wont_be_nil
-      lines.length.must_equal data.length
+        lines = @file_service.read_lines_from_file
+        
+        lines.wont_be_nil
+        lines.length.must_equal data.length
+      end
     end
   end
 end
