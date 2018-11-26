e9209e3e48e86b5a14c2e6d7837293dba3de6e74
michal_urbanski <michal_urbanski@windowslive.com>
2018-11-26 20:46:24 +0100

    More idiomatic way

--- lib/simple_tasks_ruby/models/possible_days_tester.rb
+++ lib/simple_tasks_ruby/models/possible_days_tester.rb
@@ -1,20 +1,20 @@
-class PossibleDaysTester
-  def initialize
-    @possibleDays = PossibleDays.new.days
-  end
-
-  def is_day(text)
-    lower_days = @possibleDays.map{|day| day.downcase}
-    lower_text = text.downcase
-
-    lower_days.each do |day|
-      if lower_text.start_with?(day)
-        return true
-      else
-        next # check if next element is a day
-      end
+module SimpleTasksRuby
+  class PossibleDaysTester
+    def initialize
+      @possibleDays = PossibleDays.new.days
     end
 
-    return false # if no day found than return false
+    def is_day(text)
+      lower_days = @possibleDays.map{|day| day.downcase}
+      lower_text = text.downcase
+
+      lower_days.each do |day|
+        return true if lower_text.start_with?(day)
+
+        next # Check if next element is a day
+      end
+
+      return false # If no day found than return false
+    end
   end
 end
