ba3f9e67364db8e614b6aa69b028d13497fb3a12
michal_urbanski <michal_urbanski@windowslive.com>
2018-12-16 21:35:41 +0100

    Fixes unused variables in test

--- spec/simple_tasks_ruby/other/week_spec.rb
+++ spec/simple_tasks_ruby/other/week_spec.rb
@@ -2,70 +2,67 @@ require 'spec_helper'
 
 module SimpleTasksRuby
   describe Week do
+    let(:week_name) { "Week 1"}
+    let(:week) { Week.new(week_name) }
     subject { week }
     
-    describe "Week" do
-      let(:week_name) { week_name = "Week 1"}
-      let(:week) { week = Week.new(week_name) }
+    it "Week can have name" do
+      subject.name.must_equal week_name
+    end
 
-      it "Week can have name" do
-        subject.name.must_equal week_name
+    it "Week can have days" do
+      subject.add_day(DayFactory.create_valid_day())
+
+      subject.length.must_equal 1
+    end
+
+    it "Week exposes tasks" do
+      subject.add_day(DayFactory.create_valid_day())
+      
+      subject.tasks.length.must_equal 1
+    end
+
+    it "Week exposes tasks from multiple days" do
+      subject.add_day(DayFactory.create_valid_day())
+      subject.add_day(DayFactory.create_valid_day())
+      
+      subject.tasks.length.must_equal 2
+    end
+
+    it "Week exposes tasks from multiple days and some days have no tasks" do
+      subject.add_day(DayFactory.create_valid_day())
+      subject.add_day(DayFactory.create_valid_day())
+      subject.add_day(DayFactory.create_empty_day())
+
+      subject.tasks.length.must_equal 2
+    end
+
+    it "By default not initialized week has undefined title" do
+      week = Week.new
+
+      week.name.must_equal "undefined"
+    end
+
+    it "Valid week has 7 days" do
+      days = (1..7).to_a
+
+      days.each do |day|
+        subject.add_day(day)
       end
 
-      it "Week can have days" do
-        subject.add_day(DayFactory.create_valid_day())
+      subject.is_valid.must_equal true
+      subject.length.must_equal 7
+    end
 
-        subject.length.must_equal 1
+    it "Invalid week can have less or more than 7 days" do
+      days = (1..10).to_a
+
+      days.each do |day|
+        subject.add_day(day)
       end
 
-      it "Week exposes tasks" do
-        subject.add_day(DayFactory.create_valid_day())
-        
-        subject.tasks.length.must_equal 1
-      end
-
-      it "Week exposes tasks from multiple days" do
-        subject.add_day(DayFactory.create_valid_day())
-        subject.add_day(DayFactory.create_valid_day())
-        
-        subject.tasks.length.must_equal 2
-      end
-
-      it "Week exposes tasks from multiple days and some days have no tasks" do
-        subject.add_day(DayFactory.create_valid_day())
-        subject.add_day(DayFactory.create_valid_day())
-        subject.add_day(DayFactory.create_empty_day())
-
-        subject.tasks.length.must_equal 2
-      end
-
-      it "By default not initialized week has undefined title" do
-        week = Week.new
-
-        week.name.must_equal "undefined"
-      end
-
-      it "Valid week has 7 days" do
-        days = (1..7).to_a
-
-        days.each do |day|
-          subject.add_day(day)
-        end
-
-        subject.is_valid.must_equal true
-        subject.length.must_equal 7
-      end
-
-      it "Invalid week can have less or more than 7 days" do
-        days = (1..10).to_a
-
-        days.each do |day|
-          subject.add_day(day)
-        end
-
-        subject.is_valid.must_equal false
-        subject.length.must_equal 10
-      end
+      subject.is_valid.must_equal false
+      subject.length.must_equal 10
     end
   end
 end
