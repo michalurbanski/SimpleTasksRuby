71e9a0a27222c153f9b534a9c4f8b3d0398809a4
michal_urbanski <michal_urbanski@windowslive.com>
2019-01-12 21:54:08 +0100

    Sample documentation for rdoc

--- lib/simple_tasks_ruby/models/day.rb
+++ lib/simple_tasks_ruby/models/day.rb
@@ -1,12 +1,34 @@
 module SimpleTasksRuby
+  ##
+  # Day is a container for tasks. Days are stored inside a Week container.
+  #
+  # Tasks inside a day are stored in Array.
+
   class Day
     attr_reader :name, :date, :tasks
+
+    ##
+    # Creates a new Day object.
+    #
+    # == Parameters:
+    # name::
+    #   Line read from input file. For a correct day this line consists of
+    #   a day of the week and a date, like:
+    #     Monday 2019-01-14
 
     def initialize(name)
       @name = name
       @tasks = Array.new
       @date = extract_date_from_day_name(name)
     end
+
+    ##
+    # Adds task to a day.
+    # 
+    # == Parameters:
+    # [task] object of a Task class or any of the derived classes
+    #
+    # @param task [Task] object of a Task class or any of the derived classes.
 
     def add_task(task)
       # TODO: Not a perfect solution (checking type of class) but a different handling would require Task and its hierarchy to be changed
@@ -15,9 +37,15 @@ module SimpleTasksRuby
       @tasks.push(task)
     end
 
+    ##
+    # @return [Numeric] Number of tasks in a day.
+
     def length
       @tasks.length
     end
+
+    ##
+    # Helper method to print out all the tasks in a day.
 
     def to_s
       puts(@name)
